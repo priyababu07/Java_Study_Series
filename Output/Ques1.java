c51fa798ef8fdd40108451533886fc90cf4f102f
priyababu07 <priyababu312@gmail.com>
2024-06-18 13:02:59 +0530

    Just started

---
+++ Output/Ques1.java
@@ -0,0 +1,22 @@
+/*
+Question1 : Print the pattern:
+
+*
+
+**
+
+***
+
+****
+Just to learn the output
+*/
+
+package Output;
+public class Ques1 {
+    public static void main(String [] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
---
+++ Output/main.java
@@ -0,0 +1,14 @@
+package Output;
+
+public class main {
+    public static void main(String args[]){
+        System.out.print("Hello World");
+        System.out.println("Nice to meet you");
+
+        System.out.println("Hai");
+        System.out.println("Nice to meet you");
+
+        System.out.print("Hai\n");
+        System.out.println("Choclate");
+    }
+}
