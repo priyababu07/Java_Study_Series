5d327561e5c0b9f3d9869fb2c242fb8e4dccbe03
priyababu07 <priyababu312@gmail.com>
2024-06-19 12:33:36 +0530

    changes

--- Output/main.java
+++ Output/main.java
@@ -1,7 +1,7 @@
 package Output;
 
 public class main {
-    public static void main(String args[]){
+    public static void main(String [] args){
         System.out.print("Hello World");
         System.out.println("Nice to meet you");
 
